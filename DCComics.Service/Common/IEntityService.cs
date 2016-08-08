5793f67545624a7440a6b99dd6138fc860324c9d
revanorion <revanorion@aol.com>
2016-08-08 14:00:57 -0400

    Revert "Revert "First Commit""
    This reverts commit f6b4e5601fa11c3d8ed2a03102d5ad2710a4f38d.

---
+++ DCComics.Service/Common/IEntityService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCComics.Service.Common
+{
+    public interface IEntityService<T>
+    {
+        void Create(T entity);
+        void Delete(T entity);
+        IEnumerable<T> GetAll();
+        void Update(T entity);
+    }
+}
