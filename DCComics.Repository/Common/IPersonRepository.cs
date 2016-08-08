25023d99c6af4cedeae277e35a1ee8a2846c8655
revanorion <revanorion@aol.com>
2016-08-08 13:53:09 -0400

    First Commit

---
+++ DCComics.Repository/Common/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using DCComics.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCComics.Repository.Common
+{
+    interface IPersonRepository: IGenericRepository<Person>
+    {
+        Person GetById(long id);
+    }
+}
---
+++ DCComics.Repository/PersonRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DCComics.Repository.Common;
+using System.Data.Entity;
+using DCComics.Model;
+
+namespace DCComics.Repository
+{
+    public class PersonRepository : GenericRepository<Person>, IPersonRepository
+    {
+        public PersonRepository(DbContext context)
+            : base(context)
+        {
+
+        }
+
+        public override IEnumerable<Person> GetAll()
+        {
+            return _entities.Set<Person>().AsEnumerable();
+        }
+
+        public Person GetById(long id)
+        {
+            return _dbset.Where(x => x.Id == id).FirstOrDefault();
+        }
+    }
+}
